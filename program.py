ee2efffdd77371b0a11b4c033f584206ac79cef1
Santiago Pérez <santiago.perez01@correo.usa.edu.co>
2021-11-23 15:54:30 -0500

    Added more threads to execute

--- program.py
+++ program.py
@@ -11,7 +11,7 @@ import concurrent.futures
 start=(0,0)
 def getFitnessFromNumberOfSesions(strategy,numberOfSessions,numberOfActions,perceptions):
     fitness=0
-    N=10
+    N=4
     for sesion in range(numberOfSessions):
         position=list(start)  
         board = generateBoard(N)     
@@ -28,7 +28,7 @@ def getFitnessFromNumberOfSesions(strategy,numberOfSessions,numberOfActions,perc
     return fitness
 def getFitness(strategy,cleaningSessions,numberOfActions,perceptions):
     fitness=0
-    numberOfThreads =cleaningSessions//10 + cleaningSessions%10# This must be divisor of cleaningSessions 
+    numberOfThreads =100# This must be divisor of cleaningSessions 
     with concurrent.futures.ThreadPoolExecutor() as executor:
         results= [executor.submit(getFitnessFromNumberOfSesions,
          strategy,
